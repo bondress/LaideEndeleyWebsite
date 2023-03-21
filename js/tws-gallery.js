893d50ffceab6dbfdab16716a2e94097c5d43fe2
Olayide Majoyeogbe <majoyeogbe@yahoo.com>
2023-03-21 10:05:18 +0100

    updated

---
+++ js/tws-gallery.js
@@ -0,0 +1,22 @@
+// Script to reveal scroll to top button when user scrolls down (from w3schools)
+//Get the button
+let topButton = getOne("#myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
